97abba941835653f4a5d392b1b1bea3ef427fedc
joycethh <joycethhh@gmail.com>
2023-11-13 17:07:53 -0600

    remove the app folder to the root

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,30 @@
-export default function RootLayout({
+import { Inter } from "next/font/google";
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+import './globals.css'
+
+const inter = Inter({subsets:['latin']})
+
+export const metadata = {
+  title: "Shell Splendor",
+  description: 'Created by Joyce Tang'
+}
+
+
+const RootLayout = ({
     children,
   }: {
     children: React.ReactNode
-  }) {
+  }) =>{
     return (
       <html lang="en">
-        <body>{children}</body>
+        <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+        </body>
       </html>
     )
   }
+
+  export default RootLayout;
