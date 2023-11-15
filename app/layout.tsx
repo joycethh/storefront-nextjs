ae7091188b5a20d39e8f2d7615990da87630862b
joycethh <joycethhh@gmail.com>
2023-11-14 21:11:29 -0600

    initial homepage setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import './globals.css';
 const inter = Inter({subsets:['latin']})
 
 export const metadata = {
-  title: "Shell Splendor",
+  title: "Bling Bling Bijoux",
   description: 'Created by Joyce Tang'
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+import round from "../public/images/round-pale.jpg"
+import pear from "../public/images/pear.jpg"
+import oval from "../public/images/oval.jpg"
+
 const Page = () =>{
-    return <h1 className="text-3xl font-bold underline">Hello, worked</h1>
+    return (
+        <section className="h-full">
+            {/* top-section */}
+            <div className="flex items-center shadow-md overflow-hidden ">
+            {/* left */}
+            <div className="items-center">
+                <span>Timeless Rings</span>
+                <p>Fine jewelry ready for shippment</p>
+                <span> Shop by Shapes</span>
+            </div>
+            {/* right */}
+            <div className="flex items-center">
+            <div className="flex-none w-25 h-25" >
+                <Image alt="" src={round} className="w-16 md:w-32 lg:w-48" />
+                <Link href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                <span>Shop Round</span>
+                </Link>
+            </div>
+            <div className="flex-none w-25 h-25" >
+                <Image alt="" src={oval} className="w-16 md:w-32 lg:w-48"  />
+                <Link href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                <span>Shop Oval</span>
+                </Link>
+            </div>
+            <div className="flex-none w-25 h-25" >
+                <Image alt="" src={pear} className="w-16 md:w-32 lg:w-48" />
+                <Link href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+                <span>Shop Pear</span>
+                </Link>
+            </div>
+            </div>
+            </div>
+        </section>
+    )
 }
 
 export default Page;
