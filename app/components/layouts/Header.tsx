afe9725bbb0e4605de47878091beee35b9e377fa
joycethh <joycethhh@gmail.com>
2023-11-14 22:55:28 -0600

    header basic layout

--- app/components/layouts/Header.tsx
+++ app/components/layouts/Header.tsx
@@ -1,6 +1,48 @@
+import Link from "next/link";
+import { GoPerson } from "react-icons/go";
+import { LiaShoppingBagSolid } from "react-icons/lia";
+
+
+
 const Header = () => {
     return(
-        <h1>This is Header</h1>
+        <>
+        <header className="z-10 py-10 text-violet-950"> 
+            <nav className="container flex items-center justify-between">
+                {/* logo section */}
+                <div>
+                    <Link href='/' className="text-2xl font-bold capitalize tracking-tight hover:tracking-wide">
+                        Bling Biling Bijoux
+                    </Link>
+                </div>
+                {/* nav items */}
+                <ul className="flex items-center gap-10">
+                    <li className="text-sm font-medium uppercase tracking-tight hover:tracking-wide">Rings</li>
+                    <li className="text-sm font-medium uppercase tracking-tight hover:tracking-wide">Necklace</li>
+                    <li className="text-sm font-medium uppercase tracking-tight hover:tracking-wide">Earrings</li>
+
+                   
+                </ul>
+               
+                <div>
+                <ul className="flex items-center gap-5">
+                     {/* sign in  */}
+                     <li>
+                        <Link href="/signin"> <GoPerson className="h-7 w-7"/></Link>
+                    </li>
+
+                     {/* shopping cart */}
+                    <li>
+                        <LiaShoppingBagSolid className="h-7 w-7"/>
+                    </li>
+                      
+                    </ul>
+                </div>
+
+            </nav>
+
+        </header>
+        </>
     )
 }
 
