581983307fe6645252629608372857e986bd7a0b
joycethh <joycethhh@gmail.com>
2023-12-05 21:11:10 -0600

    add the Product and the ProductList components

---
+++ app/components/products/Product.tsx
@@ -0,0 +1,42 @@
+import { StaticImageData } from "next/image";
+import  Image from "next/image";
+
+export interface IProduct {
+    id: string,
+    name: string,
+    price: number,
+    url: string,
+    description: string,
+    image: StaticImageData
+}
+
+interface IProductProps {
+    product: IProduct
+}
+
+const Product = (props: IProductProps) => {
+    return (
+        <div>
+            <h2>{props.product.name}</h2>
+            <p>{props.product.description}</p>
+            {/* image */}
+            <div>
+                <Image src={props.product.image} alt={props.product.name} />
+            </div>
+            {/* price */}
+            <div>
+            <div>${props.product.price.toFixed(2)}</div>
+                <button
+                    data-item-id={props.product.id}
+                    data-item-name={props.product.name}
+                    data-item-price={props.product.price}
+                    data-item-url={props.product.url}
+                    data-item-image={props.product.image.src}>
+                    Add to cart
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Product;
