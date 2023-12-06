43d929c35d2c17662cbe782b7e2e35046bcc1f8b
joycethh <joycethhh@gmail.com>
2023-12-06 16:46:14 -0600

    change to use the Pages router from the App router

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app';
+import Layout from '../app/components/layouts/layout';
+import "./globals.css";
+
+//define global layout;
+const App = ({Component, pageProps}: AppProps) => {
+    return <Layout>
+    <Component {...pageProps} />
+    </Layout>
+}
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+
+import Image from "next/image";
+import Link from "next/link";
+import round from "../public/images/round-pale.jpg"
+import pear from "../public/images/pear.jpg"
+import oval from "../public/images/oval.jpg"
+
+//using the Pages router; this will be the Homepage
+const Page = () => {
+    return (
+     
+        <section className="h-full">
+        {/* top-section */}
+        <div className="flex items-center shadow-md overflow-hidden">
+        {/* left */}
+        <div className="items-center">
+            <span>Timeless Rings</span>
+            <p>Fine jewelry ready for shippment</p>
+            <span> Shop by Shapes</span>
+        </div>
+        {/* right */}
+        <div className="flex items-center">
+        <div className="flex-none w-15 h-15" >
+            <Image alt="" src={round} className="w-16 md:w-32 lg:w-48" />
+            <Link href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            <span>Shop Round</span>
+            </Link>
+        </div>
+        <div className="flex-none w-15 h-15" >
+            <Image alt="" src={oval} className="w-16 md:w-32 lg:w-48"  />
+            <Link href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            <span>Shop Oval</span>
+            </Link>
+        </div>
+        <div className="flex-none w-15 h-15" >
+            <Image alt="" src={pear} className="w-16 md:w-32 lg:w-48" />
+            <Link href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            <span>Shop Pear</span>
+            </Link>
+        </div>
+        </div>
+        </div>
+    </section>
+    )
+}
+
+export default Page;
